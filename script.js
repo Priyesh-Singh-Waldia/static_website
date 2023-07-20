fb4c04e3585ee673ad1ade737cbbad36dc6a60cc
Priyesh Singh Waldia <114400261+Priyesh-Singh-Waldia@users.noreply.github.com>
2023-07-20 16:08:20 +0530

    fixed search bar visuals

---
+++ script.js
@@ -0,0 +1,17 @@
+// function dl_theme() {
+//   let body = document.querySelector("body");
+//   let theme = document.querySelector(".header");
+//   body.classList.add("d_theme");
+//   theme.classList.add("d_theme");
+// }
+
+// function dl_theme() {
+//   let theme = document.querySelector("body");
+//   if (body === "l_theme") {
+//     theme.classList.remove("l_theme");
+//     theme.classList.add("d_theme");
+//   } else {
+//     theme.classList.add("l_theme");
+//     theme.classList.remove("d_theme");
+//   }
+// }
